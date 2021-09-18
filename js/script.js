9913cca4ee2a38ad1d9d7f604f662efee01cdbc6
eliudnyamai <eliudmitau@gmail.com>
2021-09-18 18:50:44 +0300

    First push

---
+++ js/script.js
@@ -0,0 +1,86 @@
+var width_range = document.getElementById("width-range");
+var width_input = document.getElementById("width-input");
+width_input.value = width_range.value; 
+width_range.oninput = function() {
+    width_input.value = this.value
+}
+width_input.oninput = function() {
+    width_range.value = this.value
+} 
+
+var height_range = document.getElementById("height-range");
+var height_input = document.getElementById("height-input");
+height_input.value = height_range.value;
+height_range.oninput = function() {
+    height_input.value = this.value;
+} 
+height_input.oninput = function() {
+    height_range.value = this.value;
+} 
+$("#upload-form").submit(function(e) {
+    e.preventDefault(); 
+    var form = $(this);
+    var url = form.attr('action');
+    $.ajax({
+           type: "POST",
+           url: url,
+           data:  new FormData(this),
+           contentType: false,
+           cache: false,
+           processData:false,
+           beforeSend : function()
+           {
+            $('#submit-img').val('Sending...');
+           },
+           success: function(data)
+           {
+               data=JSON.parse(data)
+               if(data.success){
+                height_input.value = data.size[1];
+                width_input.value = data.size[0];
+                height_range.value = data.size[1];
+                width_range.value = data.size[0];
+                $('#uploaded_img').css('display', 'block');
+                $('#uploaded_img').attr('src',data.uploaded_file);
+                $('#submit-img').val('upload');
+                $('#resize-form').css('display', 'block');
+                $('#file-upload-error').text("")
+               }
+               else{
+                $('#file-upload-error').text(data.error)
+                $('#submit-img').val('upload');
+               }
+            }
+ });
+});
+$("#resize-form").submit(function(e) {
+    e.preventDefault(); 
+    var form = $(this);
+    var url = form.attr('action');
+    $.ajax({
+           type: "POST",
+           url: url,
+           data:  form.serialize(),
+           dataType:'json',
+           beforeSend : function()
+           {
+            $('#resize-submit').text('Resizing...');
+           },
+           success: function(data)
+           {
+                $('#resized_img').attr('src',data.resized_img);
+                $('#resized_img').css('display', 'block');
+                $('#resize-submit').text('Resize');
+                $('#download-form').css('display', 'block');
+                $('#output-image-info').text(`You have resized your image to a height of ${data.height}px and a width of ${data.width}px`);
+             }
+ });
+});
+
+$("#download-form").submit(function(e) {
+  $('#resize-form').css('display', 'none');
+  $('#download-form').css('display', 'none');
+  $('#uploaded_img').css('display', 'none');
+  $('#resized_img').css('display', 'none');
+  $('#output-image-info').text('');
+});
---
+++ php/upload.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+// May this codebase glorify God
+$target_dir = "uploads/";
+$target_file = $target_dir .$_SESSION['user'].basename($_FILES["file"]["name"]);
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$data=array();
+
+// Check if image file is a actual image or fake image
+if(!isset($_FILES["file"])) {
+  header('location: ../');
+  exit();
+}
+
+// Check file size
+if ($_FILES["file"]["size"] > 500000) {
+  $data['error']="Sorry, your file is too large.";
+  $data['success']=false;
+  echo json_encode($data);
+  exit();
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  $data['error']="Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $data['success']=false;
+  echo json_encode($data);
+  exit();  
+}
+
+if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+    $_SESSION['uploaded_file_path']='uploads/'.$_SESSION['user'].basename( $_FILES["file"]["name"]);
+    $data['success']=true;
+    $data['uploaded_file']='php/uploads/'.$_SESSION['user'].basename( $_FILES["file"]["name"]);
+    $data['size']=getimagesize('uploads/'.$_SESSION['user'].basename( $_FILES["file"]["name"]));
+    echo json_encode($data);
+    exit();
+  }
