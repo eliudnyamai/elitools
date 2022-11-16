fa103b5c568f3abf74d466ec623c8e74faaff84f
unknown <eliudmitau@gmail.com>
2022-11-16 17:12:34 +0300

    added cron

--- clear.php
+++ clear.php
@@ -3,11 +3,14 @@ include 'php/functions.php';
 $dir = 'php/uploads';
   $files = glob('php/uploads/*'."*");
   $now   = time();
+  $x=0;
   foreach ($files as $file) {
     if (is_file($file)) {
-      if ($now - filemtime($file) >= 60*60*24) { // 1 days
+      if ($now - filemtime($file) >= 60) { // 1 days
         echo filemtime($file).'<br>';
         unlink($file);
+        $x++;
       }
     }
   }
+  echo "deleted ".$x;
