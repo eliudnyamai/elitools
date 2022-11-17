b6ce5159e15857998417af1e8b3f6917565a8d58
unknown <eliudmitau@gmail.com>
2022-11-17 08:53:21 +0300

    Cron to clear all files older than a day finished

--- clear.php
+++ clear.php
@@ -3,12 +3,20 @@ include 'php/functions.php';
 $dir = 'php/uploads';
   $files = glob('php/uploads/*'."*");
   $now   = time();
-  $x=0;
   foreach ($files as $file) {
     if (is_file($file)) {
       if ($now - filemtime($file) >= 60*60*24) { // 1 days
         unlink($file);
-        $x++;
+      }
+    }
+  }
+
+  $files = glob('resize-image/php/uploads/*'."*");
+  $now   = time();
+  foreach ($files as $file) {
+    if (is_file($file)) {
+      if ($now - filemtime($file) >= 60*60*24) { // 1days
+        unlink($file);
       }
     }
   }
