baefd4688ac10a42ab28ce006394a1ce0c7ce74b
unknown <eliudmitau@gmail.com>
2023-01-10 16:18:41 +0300

    version 1.6

--- tos.php
+++ tos.php
@@ -1,42 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>Toolske.com|Terms Of Service</title>
-  <meta content="" name="Free online tools">
-  <meta content="" name="free, online, resize image, remove background,resize image online">
-  <meta property='og:title' content='Free online tools'/>
-  <meta property='og:image' content='https://toolske.com/sm-preview.png'/>
-  <meta property='og:description' content='Free Online easy and quick to use tool.'/>
-  <meta property='og:url' content='toolske.com'/>
-  <meta property='og:image:width' content='1200' />
-  <meta property='og:image:height' content='627' />
-<!-- TYPE BELOW IS PROBABLY: 'website' or 'article' or look on https://ogp.me/#types -->
-<meta property="og:type" content='website'/>
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-</head>
-
+<?php include 'includes/head.php'?>
 <body>
    <?php include 'includes/header.php';?>
    <?php include 'includes/sidebar.php';?>
