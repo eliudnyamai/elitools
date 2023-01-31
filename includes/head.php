3897fd7b7797ebe83015d8b255daa4107ce602ce
unknown <eliudmitau@gmail.com>
2023-01-31 12:47:12 +0300

    $

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,8 @@
 <?php
 function display_head($title){
  echo '<head>
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3457332264604722"
+     crossorigin="anonymous"></script>
  <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-BVTQJVB94K"></script>
 <script>
