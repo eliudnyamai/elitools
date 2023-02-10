fa51ea4a033087a856ea12d2222cad0b21b9c8d7
unknown <eliudmitau@gmail.com>
2023-02-10 12:16:28 +0300

    New homepage

--- index.php
+++ index.php
@@ -27,11 +27,11 @@ display_head('Free Online Tools','Resize an image online for free,you can resize
    <?php include 'includes/sidebar.php';?>
   <main id="main" class="main">
     <div class="pagetitle">
-      <h1>Resize Images</h1>
+      <h1>Home</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="/">Toolske.com</a></li>
-          <li class="breadcrumb-item active">Resize Images</li>
+          <li class="breadcrumb-item active">Home</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
@@ -49,174 +49,86 @@ display_head('Free Online Tools','Resize an image online for free,you can resize
             <div class="advert">
 
             </div>
-          <div style="min-height:200px;" class="col-md-12">
-           <h2>Upload your Image to resize</h2> <br>
-
-       
-
-           <!-- <form id="upload-form" action="php/upload.php" enctype="multipart/form-data" method="post">
-               <input type="file" required name="file" id="">
-               <input type="submit" value="Upload" id="submit-img" name="submit">
-           </form> <br>
-           <div id="file-upload-error"></div> -->
-           <div class="drag-area">
-    <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
-    <header>Drag & Drop to Upload File</header>
-    <span>OR</span>
-    <button class="btn">Browse File</button>
-    <input type="file" hidden>
-  </div>
-</div>
-           <div class="images">
-               <div class="input-image">
-                  <img id="uploaded_img" src="" alt="" srcset="">          
-                  <!-- image presets dropdown menu -->
-                  <div id="social-media-presets" class="social-media-presets">
-                     <div class="dropdown">
-                       <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                            <button class="btn-presets facebook  btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
-                            <i class="fa fa-facebook-square" aria-hidden="true"></i>
-                                Facebook
-                            </button>
-                        </a>
-                         <ol class="dropdown-menu">
-                            <button value="820x312" class="image-presets">820x312px(coverphoto-Desktop)</button>
-                            <button value="640x360" class="image-presets">640x360px(coverphoto-Mobile)</button>
-                            <button value="180x180" class="image-presets">180x180px(profilephoto-Desktop)</button>
-                            <button value="140x140" class="image-presets">140x140px(coverphoto-Mobile)</button>
-                            <button value="1200x630" class="image-presets">1200x630px(Banner)</button>
-                        </ol>
-                      </div>
-                      <div class="dropdown">
-                        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                            <button class="btn-presets twitter btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
-                            <i class="fa fa-twitter" aria-hidden="true"></i>
-                              Twitter
-                            </button>
-                        </a>
-                        <ul class="dropdown-menu">
-                          <button value="1500x500" class="image-presets" >1500x500px(coverphoto)</button>
-                          <button value="400x400" class="image-presets" >400x400px(profilepicture)</button>
-                          <button value="900x450" class="image-presets">900x450px(shared image)</button>
-                          <button value="440x220" class="image-presets">440x220px(instream photo)</button>
-                        </ul>
-                      </div>
-                      <div class="dropdown">
-                        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                          <button class="btn-presets linkedin btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
-                          <i class="fa fa-linkedin" aria-hidden="true"></i>
-                            Linkedin
-                          </button>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <button value="1584x396" class="image-presets">1584x396px(coverphoto-personal)</button>
-                            <button value="400x400" class="image-presets">400x400px(profilephoto-personal)</button>
-                            <button value="1128x191" class="image-presets" >1128x191px(coverphoto-company)</button>
-                            <button value="300x300" class="image-presets">300x300px(profilephoto-company)</button>
-                            <button value="1350x440" class="image-presets">1350x440px(Blogpostimage)</button>
-                            <button value="1128x376" class="image-presets">1128x376px(Hero-company)</button>
-                            <button value="300x300" class="image-presets">300x300px(profilephoto-company)</button>
-                            <button value="502x282" class="image-presets">502x282px(Moduleimage-company)</button>
-                            <button value="300x300" class="image-presets">300x300px(profilephoto-company)</button>
-                            <button value="900x600" class="image-presets">900x600px(companyphotos-company)</button>
-                        </ul>
-                      </div>
-                      <div class="dropdown">
-                          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                          <button class="btn-presets tumblr btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
-                          <i class="fa fa-tumblr" aria-hidden="true"></i>
-                              Tumblr
-                          </button>
-                          </a>
-                          <ul class="dropdown-menu">
-                            <button value="128x128" class="image-presets" >128x128px(profilephoto)</button>
-                            <button value="500x750" class="image-presets">500x750px(ImagePost)</button>
-                          </ul>
-                      </div>
-                      <div class="dropdown">
-                        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                          <button class="btn-presets btn-primary youtube dropdown-toggle" type="button" data-toggle="dropdown">
-                          <i class="fa fa-youtube-play" aria-hidden="true"></i>
-                              Youtube
-                          </button>
-                        </a>
-                        <ul class="dropdown-menu">
-                          <button value="800x800" class="image-presets">800x800px(ChannelIcon)</button>
-                          <button value="2500x1440" class="image-presets">2560x1440(coverphoto)</button>
-                        </ul>
-                      </div>
-                        <div class="dropdown">
-                          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                            <button class="btn-presets btn-primary pintrest dropdown-toggle" type="button">
-                              <i class="fa fa-pinterest" aria-hidden="true"></i>
-                              Pintrest
-                              </button>
-                          </a>
-                          <ul class="dropdown-menu">
-                            <button value="165x165" class="image-presets">165x165PX(profileImage)</button>
-                            <button value="222x150" class="image-presets">222x150px(BoardImageSize)</button>
-                          </ul>
-                        </div>
-                      </div>
-                      <form method="post" action="php/resize.php" id="resize-form" class="">
-                        <p>Choose image width in px:</p>
-                        <div class="img-dimensions" >
-                            <input type="range"  min="1" max="3000" value="50" class="slider" id="width-range">
-                            <input type="number" name="width"  id="width-input">
-                        </div>
-                        <p>Choose image height in px:</p>
-                        <div class="img-dimensions" >
-                            <input type="range"  min="1" max="3000" value="50" class="slider" id="height-range">
-                            <input type="number" name="height" id="height-input">
-                        </div>
-                        <input id="resize-submit" class="resize-btn btn" name="resize" value="resize image" type="submit">
-                      </form>
+            <!-- Default Card -->
+            <div class=" col-12 mt-5">
+            <div class="card-body">
+              <h5 class="animated  card-title">Welcome To Toolske.com!!</h5>
+              <div class="row">
+                  <div class="col-3">
+                  <!-- Card with an image on bottom -->
+                  <div class="">
+                  <img src="assets/img/time.png" class="card-img-bottom" alt="...">
+                    <h5 class="card-title text-center">Time Saving</h5>
+                  </div><!-- End Card with an image on bottom -->
                   </div>
-                  <div  class="output-image">
-                        <div class="output-image-container">
-                            <img id="resized_img" src="" alt="" srcset="">
-                        </div>
-                        <div id="output-image-info"></div>
-                        <form id="download-form" method="post" action="php/download.php">
-                            <button class="download-btn btn" type="submit">Download</button>    
-                        </form>
-                    </div>
-           </div>    
+                  <div class="col-3">
+                       <!-- Card with an image on bottom -->
+                  <div class="">
+                  <img src="assets/img/money.png" class="card-img-bottom" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">Free</h5>
+                  </div>
+                  </div><!-- End Card with an image on bottom -->
+                  </div>
+                  <div class="col-3">
+                   <!-- Card with an image on bottom -->
+                   <div class="">
+                  <img src="assets/img/quick.png" class="card-img-bottom" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">Quick</h5>
+                  </div>
+                  </div><!-- End Card with an image on bottom -->
+                  </div>
+                  <div class="col-3">
+                       <!-- Card with an image on bottom -->
+                  <div class="">
+                  <img src="assets/img/easy.png" class="card-img-bottom" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">Easy</h5>
+                  </div>
+                  </div><!-- End Card with an image on bottom -->
+                  </div>
+              </div>
+            </div>
+          </div><!-- End Default Card -->
+          <div class="row container">
+            <div class="col-12 col-md-6 col-lg-4">
+                <!-- Card with titles, buttons, and links -->
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title animated">Free Online Image Resizer</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Resize Images</h6>
+              <p class="card-text">Resize all types of images you want online. Use the preset social media image sizes to make your work fast.</p>
+              <p class="card-text"><a href="/resize-image" class="btn btn-primary">Resize Now!!</a></p>
+            </div>
+          </div><!-- End Card with titles, buttons, and links -->
+              </div>
+              <div class="col-12 col-md-6 col-lg-4">
+                <!-- Card with titles, buttons, and links -->
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title animated">Free Text To SQL Converter</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Convert Text To SQL</h6>
+              <p class="card-text">English text to SQL for free. Describe what you would like your query to do</p>
+              <p class="card-text"><a href="/text2sql" class="btn btn-primary">Convert Now!!</a></p>
+            </div>
+          </div><!-- End Card with titles, buttons, and links -->
+              </div>
+              <div class="col-12 col-md-6 col-lg-4">
+                <!-- Card with titles, buttons, and links -->
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title animated">Any format converter</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Convert Images</h6>
+              <p class="card-text">Convert images to various formats for free online. Eg png to jpg, png to bmp, tiff to png etc.</p>
+              <p class="card-text"><a href="#" class="btn btn-primary" disabled>Coming Soon</a></p>
+            </div>
+          </div><!-- End Card with titles, buttons, and links -->
+              </div>
+              <div class=" p-3 d-flex flex-row-reverse container-fluid blog-cto">
+              <button id="breathing-button" class="floatleft"><a href="https://toolske.com/blog/">VISIT BLOG</a></button>
+              </div> 
           </div>
-        </div><!-- End Reports -->
-        <div>
-        <h2>How To Resize Images Online For Free-No Sign Up Needed</h2>
-<p>Various kinds of people will need an image resizing tool for their various purposes such as posting to social media among others. To resize an image for free online you can use a free image resizing tool provide here at toolske.com. Toolske.com is a website providing a collection of free online tools to help you achieve faster.</p>
-<h3>To resize an image:</h3>
-<ol>
-<li>	From you browser access toolske.com</li>
-<li>	Click on Resize images from the menu on your left</li>
-<li>	Upload your image that you want to resize.</li>
-<li>Select the height and width you would like to resize your image to</li>
-<li>	Click "Resize" and your resized image will appear on the screen ready for download</li>
-<li>	Click on “Download” image. You will find your image inside your downloads folder</li>
-</ol>
-<h3>Supported Image types by the resizing tool:</h3>
-<ol>
-<li>	JPEG (or JPG) - Joint Photographic Experts Group</li>
-<li>	PNG - Portable Network Graphics</li>
-<li>	JFIF- JPEG File Interchage Format</li>
-</ol>
-
-<p>The tool also provides predefined image sizes for your various social media needs. You just need to select the social media site of your choice and select the image size you woold like from the dropdown. For example to resize your image  for upload as a facebook cover photo, you just need to click the facebook dropdown and select the cover photo option then click resize.
-You can resize your image for free for all your social media needs. The following social media sites are supported:
-</p>
-<ol>
-<li>Facebook</li>
-<li>YouTube</li>
-<li>Tweeter</li>
-<li>Linkedin</li>
-<li>Tumblr</li>
-<li>Pinterest</li>
-</ol>
-<p><strong>All your uploads are deleted automatically after 24 hours. We do not keep them.</strong></p>
-
-        </div>
        </div>
       </div><!-- End Left side columns -->
     </div>
