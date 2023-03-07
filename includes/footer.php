40bc1bd68b8cd30131eac438b57bb5856cff081e
unknown <eliudmitau@gmail.com>
2023-03-07 11:11:54 +0300

    c

--- includes/footer.php
+++ includes/footer.php
@@ -19,7 +19,24 @@
   <script src="assets/vendor/php-email-form/validate.js"></script>
   <script src="js/script.js"></script>
   <script src="js/contact.js"></script>
-  <script src="js/dragndrop.js"></script>
+  <script>
+  (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "fmv86ipaz6");
+</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3457332264604722"
+crossorigin="anonymous"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BVTQJVB94K"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+
+gtag("config", "G-BVTQJVB94K");
+</script>
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>';
 ?>
--- includes/head.php
+++ includes/head.php
@@ -2,17 +2,6 @@
 function display_head($title,$desc){
  echo '<head>
  <script src="js/dragndrop.js"></script>
- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3457332264604722"
-     crossorigin="anonymous"></script>
- <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BVTQJVB94K"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag("js", new Date());
-
-  gtag("config", "G-BVTQJVB94K");
-</script>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <title>'.$title.'</title>
@@ -28,14 +17,6 @@ function display_head($title,$desc){
   <meta http-equiv="content-language" content="en-gb">
 <!-- TYPE BELOW IS PROBABLY: "website" or "article" or look on https://ogp.me/#types -->
 <meta property="og:type" content="website"/>
-<script>
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "fmv86ipaz6");
-</script>
-
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
