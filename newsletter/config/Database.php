8553c94820a067c9bc73485c86d7de7f73b7e3d0
unknown <eliudmitau@gmail.com>
2023-03-20 13:10:30 +0300

    Newslettter

---
+++ newsletter/config/Database.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+class Database{
+	
+	private $host  = 'localhost';
+    private $user  = 'root';
+    private $password   = "nyamai";
+    private $database  = "toolske"; 
+    
+    public function getConnection(){		
+		$conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+		if($conn->connect_error){
+			die("Error failed to connect to MySQL: " . $conn->connect_error);
+		} else {
+			echo "connected";
+			//return $conn;
+		}
+    }
+}
+?>
---
+++ newsletter/index.php
@@ -0,0 +1,38 @@
+<?php 
+include('inc/header.php');
+?>
+<title>webdamn.com : Demo Build Newsletter System with PHP and MySQL</title>
+<script src="js/subscribe.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<?php include('inc/container.php');?>
+<div class="content"> 
+	<div class="container-fluid">
+		<h2>Example: Build Newsletter System with PHP and MySQL</h2>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="main-content">
+					<div class="susbcribe-container">
+						<div class="top">
+							<h2>Subscribe Now</h2>
+							<h3>Enroll for our <span>free</span> updates</h3>
+							<li>We will send you weekly updates</li>
+							<li>Get daily articles and latest news</li>
+						</div>
+						<div class="bottom">							
+							<div class="status"></div>						
+							<form action="subscription.php" id="subscribeForm" method="post">							
+								<input type="text" class="form-control" id="name" placeholder="Full Name" required="">	
+								<span class="text-danger hidden" id="nameError"></span>
+								<input type="email" class="form-control" id="email" placeholder="E-mail" required="">
+								<span class="text-danger hidden" id="emailError"></span>
+								<input type="button" id="subscribe" value="Subscribe Now">
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	 
+	</div>       
+</div>   		
+<?php include('inc/footer.php');?>
+
