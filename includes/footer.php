a8d7677c2ec9f394f881babf8ddf0711ba30960f
unknown <eliudmitau@gmail.com>
2023-03-22 20:19:42 +0300

    newlt

--- includes/footer.php
+++ includes/footer.php
@@ -22,6 +22,7 @@
   <script src="js/script.js"></script>
   <script src="js/contact.js"></script>
   <script src="newsletter/js/subscribe.js"></script>
+  <script src="../newsletter/js/subscribe.js"></script>
   <script>
   (function(c,l,a,r,i,t,y){
       c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
--- newsletter/config/Database.php
+++ newsletter/config/Database.php
@@ -4,7 +4,7 @@ class Database{
 	
 	private $host  = 'localhost';
     private $user  = 'root';
-    private $password   = "nyamai";
+    private $password   = "";
     private $database  = "toolske"; 
     
     public function getConnection(){		
