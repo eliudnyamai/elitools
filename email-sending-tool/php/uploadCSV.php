9c2b3e9f85d95fa882e4b2cce418b9fb86e8bf1a
unknown <eliudmitau@gmail.com>
2023-04-24 22:25:31 +0300

    grayscale

--- email-sending-tool/php/uploadCSV.php
+++ email-sending-tool/php/uploadCSV.php
@@ -8,9 +8,14 @@ define ('SITE_ROOT', realpath(dirname(__FILE__)));
           $index=0;
           while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
            {
-            $name=utf8_encode(trim($getData[0]))." ".utf8_encode(trim($getData[1]));
+            if(empty($getData[1])){
+               $name=trim($getData[0]);
+            }
+            else{
+               $name=utf8_encode(trim($getData[0]))." ".utf8_encode(trim($getData[1]));
+            }
                    $stmt = $pdo->prepare("INSERT INTO users (name, email) VALUES (:name, :email)");
-                   $stmt->bindParam(':name', preg_quote($name));
+                   $stmt->bindParam(':name', trim($name));
                    $stmt->bindParam(':email', utf8_encode($getData[2]));
                    $stmt->execute();  
                    $index++; 
