d7d19f077f1aafa61d813f149beffcc21f1b4dc4
eliudnyamai <eliudmitau@gmail.com>
2023-08-14 15:07:16 +0300

    recaptcha

--- text2sql/js/script.js
+++ text2sql/js/script.js
@@ -63,11 +63,15 @@ $('#sql-query').keypress(function(event) {
               $('#generated-sql').text(data.sql);  
               $('#generate-sql').text('Generate SQL!');
               $('#sql-query').text("");  
+              $('#generate-sql').attr('disabled', true);
+              grecaptcha.reset();
             }
             else{
               console.log(data);
               $('#generated-sql').text(data.error);  
               $('#generate-sql').text('Try again');
+              $('#generate-sql').attr('disabled', true);
+              grecaptcha.reset();
             }  
             }
 
